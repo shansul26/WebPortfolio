e9ee4038f9c98c3ffd6936f66e8bf3b185f604c5
Shannon <sullivsm@lafayette.edu>
2016-12-04 20:09:42 -0500

    responsive-about-page
    Adding code to make the “about” page responsive.

--- js/myScript.js
+++ js/myScript.js
@@ -1,8 +1,6 @@
 //not sure what I'll put here... maybe some fun animations? I may try to do that in CSS if I can...
 
 $(document.body).on("click", ".portfolio-column", function() {
-    console.log("Hello, world!");
-    console.log(this.id);
     window.location = this.id + ".html";
 });
 
